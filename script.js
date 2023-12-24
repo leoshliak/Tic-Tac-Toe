88620430568d67afa3b3ec9c0802323fdf832762
leoshliak <levshlyak@gmail.com>
2023-12-24 14:56:57 +0200

    final version

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const GameBoard = (function(){
-    const boardArray = ['', '', '', '', '', '', '', '', '',];
+    let boardArray = ['', '', '', '', '', '', '', '', '',];
     let gameOver = false;
   const squares = document.querySelectorAll('.square');
+  const turn1String = document.querySelector('.turn1');
+  const turn2String = document.querySelector('.turn2');
+  const restartBtn = document.querySelector('.restart');
+  const player1Title = document.querySelector('.player1');
+  const player2Title = document.querySelector('.player2');
   const player1 = {
     marker: 'X',
     hisTurn: true,
@@ -17,18 +22,14 @@ const GameBoard = (function(){
      console.log(boardArray)
    },
    turnFunct: function(){
-    const turn1String = document.querySelector('.turn1');
-  const turn2String = document.querySelector('.turn2');
-  const player1Title = document.querySelector('.player1');
-  const player2Title = document.querySelector('.player2');
-    if(player1.hisTurn == true && player2.hisTurn == false){
+    if(player1.hisTurn == true && player2.hisTurn == false && gameOver == false){
    player1.hisTurn = false;
    player2.hisTurn = true;
    turn1String.style.opacity = 0;
    turn2String.style.opacity = 1;
    player1Title.classList.add('notYourTurn');
    player2Title.classList.remove('notYourTurn');
-}else if(player1.hisTurn == false && player2.hisTurn == true){
+}else if(player1.hisTurn == false && player2.hisTurn == true && gameOver == false){
     player1.hisTurn = true;
     player2.hisTurn = false;
     turn1String.style.opacity = 1;
@@ -53,15 +54,39 @@ const GameBoard = (function(){
     ];
     for (let i = 0; i < winningComb.length; i++) {
       const [a, b, c] = winningComb[i];
-      
       if (board[a] && board[a] === board[b] && board[b] === board[c]) {
-        alert(`Player ${board[a]} Won`);
-        
+        if(board[a] == 'X'){
+          turn1String.textContent = 'Player 1 Won!';
+        }else if(board[a] == 'O'){
+          turn2String.textContent = 'Player 2 Won!'
+        }
         return gameOver = true;
       }
     }
   },
+  restart: function(){
+    squares.forEach(function(element) {
+      element.textContent = '';
+  });
+    boardArray = ['', '', '', '', '', '', '', '', '',];
+    gameOver = false;
+    player1.hisTurn = true;
+    player2.hisTurn = false;
+    turn1String.style.opacity = 1;
+    turn2String.style.opacity = 0;
+    player1Title.classList.remove('notYourTurn');
+    player2Title.classList.add('notYourTurn');
+    turn1String.textContent = "Player 1's turn";
+    turn2String.textContent = "Player 2's turn";
+    console.log(boardArray)
+  },
  }
+
+ restartBtn.addEventListener('click',() =>{
+  playRound.restart();
+  
+ });
+
   squares.forEach(addEventListener('click', function(e){
     if(!e.target.classList.contains('square'))return;
     if(!e.target.textContent == '')return;
